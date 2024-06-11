4221d01e85a630b4f590a51477a3d86a9d9c7580
Asik8 <motasembillah84@gmail.com>
2024-06-11 15:17:03 +0600

    p6 done

---
+++ Practice Problems/p5.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n;
+        cin >> n;
+        ll ar[n];
+        for (int i = 0; i < n; i++)
+            cin >> ar[i];
+    }
+
+    return 0;
+}
