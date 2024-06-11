217fc4bc38d21312db0edcfbc3c9414f8b3bf528
Asik8 <motasembillah84@gmail.com>
2024-06-11 09:56:25 +0600

    updated

--- 8_VectorReplace.cpp
+++ 8_VectorReplace.cpp
@@ -2,9 +2,9 @@
 using namespace std;
 #define ll long long int
 
-int main() {
+int main() 
+{
     vector<int> v = {1,3,5,2,4,6,8,4,2,5,9,4,2,4,4,2,5,4,2,2};
-
     replace(v.begin(),v.end()-1,2,200);
     for(int x:v)
         cout<<x<<" ";
