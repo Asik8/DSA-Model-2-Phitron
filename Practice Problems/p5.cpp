21cdffb15b29f24c40ac0b68dfe6d99a61275cb5
Asik8 <motasembillah84@gmail.com>
2024-06-11 15:58:06 +0600

    5th problem done

--- Practice Problems/p5.cpp
+++ Practice Problems/p5.cpp
@@ -2,19 +2,38 @@
 using namespace std;
 #define ll long long int
 
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    int t;
-    cin >> t;
+int main()
+{
+    int n;
+    cin>>n;
+    vector<int> v(n);
+    for(int i=0;i<n;i++)
+        cin>>v[i];
+    int val,new_val;
+    cin>>val>>new_val;
 
-    while (t--) {
-        int n;
-        cin >> n;
-        ll ar[n];
-        for (int i = 0; i < n; i++)
-            cin >> ar[i];
+    for (int i = 0; i < n; i++)
+        if(v[i] == val) v[i] = new_val;
+
+    for (int i = 0; i < n; i++)
+        cout<<v[i]<<" ";
+    cout<<endl; 
+
+    int find_val;
+    cin>>find_val;
+    bool f = false;
+    int ind = 0;
+    for(int i=0;i<n;i++)
+    {
+        if(v[i] == find_val)
+        {
+            f = true;
+            ind = i;
+            break;
+        }
     }
+    if(f) cout<<ind<<endl;
+    else cout<<"Value not found\n";
 
     return 0;
 }
